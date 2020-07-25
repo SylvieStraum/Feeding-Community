7acee44cdfc888fb64d0a410870f63cefe0c969a
Reelshrimpboat <Camneelycreative@gmail.com>
2020-07-25 12:25:03 -0500

    Menu PUT and Approve only PUT functional

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -17,7 +17,7 @@ router.get('/', rejectNotAdmin, (req, res) => {
         });
 });
 
-// GET ROUTE for selecting single user
+// GET ROUTE for selecting single user info
 router.get('/user/:id', rejectNotAdmin, (req, res) => {
     const queryText = `SELECT * FROM "user"
                         WHERE "id" = $1`;
@@ -32,9 +32,7 @@ router.get('/user/:id', rejectNotAdmin, (req, res) => {
         });
 });
 
-/**
- * Put route template
- */
+//PUT ROUTE to adjust all account info
 router.put('/user/:id', rejectNotAdmin, (req, res) => {
     
     console.log('body:', req.body)
@@ -46,7 +44,7 @@ router.put('/user/:id', rejectNotAdmin, (req, res) => {
                         WHERE "user_id" = $16;`;
 
     const values = [req.body.first_name, req.body.last_name, req.body.email_address, req.body.date_of_birth, req.body.annual_income, req.body.building_address1, req.body.building_address2, req.body.zip_code, req.body.county_id, req.body.city, req.body.meal_choice, req.body.special_request, req.body.dietary_restrictions, req.body.approval_status, req.body.days, req.params.id];
-    console.log('post request, values:', values)
+    console.log('put request, values:', values)
     pool.query(queryText, values)
         .then((results) => {
             res.send(results);
@@ -54,6 +52,48 @@ router.put('/user/:id', rejectNotAdmin, (req, res) => {
             console.log(error);
             res.sendStatus(500);
     })
+});
+
+//PUT ROUTE to approve single user
+router.put('/approve/:id', rejectNotAdmin, (req, res) => {
+
+    console.log('body:', req.body)
+    const queryText = `UPDATE dependents
+                        SET  ("approval_status")
+                        = ($1)
+                        WHERE "user_id" = $2;`;
+
+    const values = [req.body.approval_status, req.params.id];
+    console.log('put request, values:', values)
+    pool.query(queryText, values)
+        .then((results) => {
+            res.send(results);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        })
+});
+
+//PUT ROUTE to change menu
+router.put('/menu/', rejectNotAdmin, (req, res) => {
+
+    console.log('body:', req.body)
+    const queryText = `UPDATE "menu" AS m
+                            SET  "description" = c."description"
+                            FROM (VALUES
+                            (1, $1),(2, $2),(3, $3))
+                            AS c("id", "description")
+                            WHERE c."id" = m."id" ;`;
+
+    const values = [req.body.option1, req.body.option2, req.body.veggie];
+    console.log('put request, values:', values)
+    pool.query(queryText, values)
+        .then((results) => {
+            res.send(results);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        })
 });
 
 module.exports = router;
