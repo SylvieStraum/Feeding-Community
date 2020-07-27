90dceb49df6f80ea68431c4f17ecc1e12068312f
Reelshrimpboat <Camneelycreative@gmail.com>
2020-07-27 10:50:35 -0500

    added back phone number

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -69,13 +69,13 @@ router.put('/user/:id', rejectNotAdmin, (req, res) => {
     
     console.log('body:', req.body)
     const queryText = `UPDATE dependents
-                        SET  ("first_name", "last_name", "email_address", "date_of_birth", "annual_income",
+                        SET  ("first_name", "last_name", "email_address", "date_of_birth", "annual_income", "phone_number",
                         "building_address1", "building_address2", "zip_code", "county_id", "city", "meal_choice",
                         "special_request", "dietary_restrictions", "approval_status", "days")
-                        = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
-                        WHERE "user_id" = $16;`;
+                        = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
+                        WHERE "user_id" = $17;`;
 
-    const values = [req.body.first_name, req.body.last_name, req.body.email_address, req.body.date_of_birth, req.body.annual_income, req.body.building_address1, req.body.building_address2, req.body.zip_code, req.body.county_id, req.body.city, req.body.meal_choice, req.body.special_request, req.body.dietary_restrictions, req.body.approval_status, req.body.days, req.params.id];
+    const values = [req.body.first_name, req.body.last_name, req.body.email_address, req.body.date_of_birth, req.body.annual_income, req.body.phone_number, req.body.building_address1, req.body.building_address2, req.body.zip_code, req.body.county_id, req.body.city, req.body.meal_choice, req.body.special_request, req.body.dietary_restrictions, req.body.approval_status, req.body.days, req.params.id];
     console.log('put request, values:', values)
     pool.query(queryText, values)
         .then((results) => {
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -28,13 +28,13 @@ router.post('/register', (req, res, next) => {
                         ($1, $2, 1)
                         RETURNING id )
                         INSERT INTO "dependents"
-                        ( "user_id", "first_name", "last_name", "email_address", "date_of_birth", "annual_income",
+                        ( "user_id", "first_name", "last_name", "email_address", "date_of_birth", "annual_income", "phone_number",
                           "building_address1", "building_address2", "zip_code", "county_id", "city", "meal_choice",
                           "special_request", "dietary_restrictions", "approval_status", "days")
-                        SELECT insert1.id, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, 'false', $16
+                        SELECT insert1.id, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, 'false', $17
                         FROM insert1                      
                         ;`;
-  const values = [username, password, bod.first_name, bod.last_name, bod.email_address, bod.date_of_birth, bod.annual_income, bod.building_address1, bod.building_address2, bod.zip_code, bod.county_id, bod.city, bod.meal_choice, bod.special_request, bod.dietary_restrictions, bod.days]
+  const values = [username, password, bod.first_name, bod.last_name, bod.email_address, bod.date_of_birth, bod.annual_income, bod.phone_number, bod.building_address1, bod.building_address2, bod.zip_code, bod.county_id, bod.city, bod.meal_choice, bod.special_request, bod.dietary_restrictions, bod.days]
   
   console.log('query:', queryText, "values:", values)
 
