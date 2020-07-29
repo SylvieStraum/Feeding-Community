fd63b2e3a948043d47a7b768b5340ee8fcdde248
Reelshrimpboat <Camneelycreative@gmail.com>
2020-07-29 12:32:03 -0500

    orders ssave works for current date

--- server/routes/orders.router.js
+++ server/routes/orders.router.js
@@ -7,7 +7,9 @@ const encryptLib = require('../modules/encryption');
 // GET ROUTE to get all orders
 router.get('/', rejectNotAdmin, (req, res) => {
 
-    const queryText = `SELECT * FROM "orders";`;
+    const queryText = `SELECT * FROM "orders"
+                        JOIN "dependents" ON "orders"."dependent_id" = "dependents"."id"
+                        ;`;
     pool.query(queryText)
         .then((result) => {
             console.log(`GET Ratings database request successful`, result);
@@ -58,13 +60,13 @@ router.get('/total/today/', rejectNotAdmin, (req, res) => {
 router.put('/save-day/', rejectNotAdmin, (req, res) => {
     
     let values = [];
+    let today = new Date();
+    let date = '';
     if(req.body.columnName){
     columnName = req.body.columnName
     values = [columnName, columnName];
     } 
     else{
-    let today = new Date();
-    let date = '';
     if (today.getMonth() < 10) {
         if (today.getDate() < 10) {
             date = today.getFullYear() + '-0' + (today.getMonth() + 1) + '-0' + today.getDate() + '-' + 'orders';
@@ -81,18 +83,16 @@ router.put('/save-day/', rejectNotAdmin, (req, res) => {
     values = [date, date];
     }
 
-    console.log('column:', columnName);
-
     const queryText = `BEGIN;
-                       ALTER TABLE orders ADD COLUMN $1 INT;
+                       ALTER TABLE orders ADD COLUMN "${date}" INT;
                        UPDATE "orders"
-                       SET $2 = "current_meal"."number_of_meals"
+                       SET "${date}" = "current_meal"."number_of_meals"
                        FROM "current_meal" WHERE orders.dependent_id = current_meal.dependent_id;
                        COMMIT;`;
 
     
-    console.log('put request: values:', values)
-    pool.query(queryText, values)
+    console.log('put request:,', queryText, ' values:', values)
+    pool.query(queryText)
         .then((results) => {
             res.send(results);
         }).catch((error) => {
