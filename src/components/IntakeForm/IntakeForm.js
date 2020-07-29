1abec7397d48fc6e95a8d18811753eaf2d99cc66
Jahan Powell <jahanpowell23@gmail.com>
2020-07-29 14:14:23 -0500

    IntakeForm functionality

--- src/components/IntakeForm/IntakeForm.js
+++ src/components/IntakeForm/IntakeForm.js
@@ -13,7 +13,7 @@ class IntakeForm extends Component {
         building_address2: '',
         city: '',
         zip_code: '',
-        county_id: '',
+        county_id: 0,
         date_of_birth: '',
         special_request: '',
         dietary_restrictions: '',
@@ -21,10 +21,42 @@ class IntakeForm extends Component {
         referral_id: 1
     }
 
+    componentDidMount = () => {
+        this.getCounty();
+        this.getOrg();
+    }
+
+    getCounty = () => {
+        this.props.dispatch({
+            type: 'GET_COUNTIES',
+        })
+    }
+
+    getOrg = () => {
+        this.props.dispatch({
+            type: 'GET_ORGS',
+        })
+    }
+
     createDependent = (event) => {
         event.preventDefault();
         console.log('this.state', this.state)
-        this.props.dispatch({type: 'POST_NEW_DEPENDENT', payload: this.state});
+        this.props.dispatch({ type: 'POST_NEW_DEPENDENT', payload: this.state });
+        this.setState({
+            first_name: '',
+            last_name: '',
+            phone_number: '',
+            building_address1: '',
+            building_address2: '',
+            city: '',
+            zip_code: '',
+            county_id: 0,
+            date_of_birth: '',
+            special_request: '',
+            dietary_restrictions: '',
+            meal_choice: 1,
+            referral_id: 1
+        })
 
     }
 
@@ -34,145 +66,164 @@ class IntakeForm extends Component {
         })
     }
 
-    render(){
+    render() {
         console.log(this.state)
-        return(
+        return (
             <>
-            <h1>Sign Up</h1>
-            <p>Enter client information form</p>
-            <div>
-                <form onSubmit={this.createDependent}>
-                    <p>Please enter client information:</p>
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="First Name"
-                        onChange={(event) => this.handleInputs(event, "first_name")}
+                <h1>Sign Up</h1>
+                <p>Enter client information form</p>
+                <div>
+                    <form onSubmit={this.createDependent}>
+                        <p>Please enter client information:</p>
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            placeholder="First Name"
+                            value={this.state.first_name}
+                            onChange={(event) => this.handleInputs(event, "first_name")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Last Name"
-                        onChange={(event) => this.handleInputs(event, "last_name")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.last_name}
+                            placeholder="Last Name"
+                            onChange={(event) => this.handleInputs(event, "last_name")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Phone Number"
-                        onChange={(event) => this.handleInputs(event, "phone_number")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.phone_number}
+                            placeholder="Phone Number"
+                            onChange={(event) => this.handleInputs(event, "phone_number")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Address 1"
-                        onChange={(event) => this.handleInputs(event, "building_address1")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.building_address1}
+                            placeholder="Address 1"
+                            onChange={(event) => this.handleInputs(event, "building_address1")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Address 2"
-                        onChange={(event) => this.handleInputs(event, "building_address2")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.building_address2}
+                            placeholder="Address 2"
+                            onChange={(event) => this.handleInputs(event, "building_address2")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="City"
-                        onChange={(event) => this.handleInputs(event, "city")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.city}
+                            placeholder="City"
+                            onChange={(event) => this.handleInputs(event, "city")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Zip Code"
-                        onChange={(event) => this.handleInputs(event, "zip_code")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.zip_code}
+                            placeholder="Zip Code"
+                            onChange={(event) => this.handleInputs(event, "zip_code")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="County"
-                        onChange={(event) => this.handleInputs(event, "county_id")}
-                        />
+                        <label>Select County</label>
+                        <select
+                            required
+                            type="text"
+                            value={this.state.county_id}
+                            placeholder="select county"
+                            type="dropdown"
+                            onChange={(event) =>
+                                this.handleInputs(event, "county_id")
+                            }>
+                            <option value="0"></option>
+                            {this.props.counties.map((item) => ( 
+                                <option value={item.id}>{item.county_name}</option>
+                            ))}
+                        </select>
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Date of Birth"
-                        onChange={(event) => this.handleInputs(event, "date_of_birth")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.date_of_birth}
+                            placeholder="Date of Birth"
+                            onChange={(event) => this.handleInputs(event, "date_of_birth")}
                         />
                         <br />
                         <p>Meal Type:</p>
                         <select
                             type="dropdown"
+                            value={this.state.meal_choice}
                             onChange={(event) => this.handleInputs(event, "meal_choice")
-                            
-                        }>
+
+                            }>
+                            <option value="0"></option>
                             <option value="1">Meat</option>
                             <option value="2">2nd Meat</option>
                             <option value="3">Veggie</option>
                             <option value="4">Special Request</option>
                         </select>
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Special Requests"
-                        onChange={(event) => this.handleInputs(event, "special_request")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.special_request}
+                            placeholder="Special Requests"
+                            onChange={(event) => this.handleInputs(event, "special_request")}
                         />
                         <br />
-                    <label></label>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Dietary Restrictions"
-                        onChange={(event) => this.handleInputs(event, "dietary_restrictions")}
+                        <label></label>
+                        <input
+                            required
+                            type="text"
+                            value={this.state.dietary_restrictions}
+                            placeholder="Dietary Restrictions"
+                            onChange={(event) => this.handleInputs(event, "dietary_restrictions")}
                         />
                         <br />
                         <p>Referral Organiztation:
                         </p>
                         <select
                             type="dropdown"
-                            onChange={(event) => 
-                            this.handleInputs(event, "referral_id")
+                            value={this.state.referral_id}
+                            onChange={(event) =>
+                                this.handleInputs(event, "referral_id")
                             }>
-                            <option value="1">Minneapolis Public Housing Agency</option>
-                            <option value="2">Commonbond Communities</option>
-                            <option value="3">Lakes Day Care</option>
-                            <option value="4">Ebyan ADC</option>
-                            <option value="5">MN Senior Center</option>
-                            <option value="6">Nurturing Hands Day Center</option>
-                            <option value="7">Umatul Islam</option>
+                            
+                            {this.props.organizations.map((item, i) => (
+                                <option value={item.id}>{item.referral_name}</option>
+                            ))}
                         </select>
                         <br />
                         <button className="Next Step" onClick={this.createDependant}>Next Step</button>
 
                     </form>
-            </div>
-            
+                </div>
+
             </>
-          
-          
+
+
         )
     }
 }
 
 const mapStateToProps = (reduxState) => ({
-    reduxState,
-  });
-  
+    counties: reduxState.counties,
+    organizations: reduxState.organizations
+});
+
 
 export default connect(mapStateToProps)(IntakeForm);
