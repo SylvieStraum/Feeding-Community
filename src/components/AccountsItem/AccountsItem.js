5a721608b910b6c4262ea2a2ff2fc58bca2124b5
JaydenThunberg <thunberg.jayden@gmail.com>
2020-07-30 11:05:47 -0500

    all table cells editable

--- src/components/AccountsItem/AccountsItem.js
+++ src/components/AccountsItem/AccountsItem.js
@@ -20,7 +20,6 @@ class AccountsItem extends Component {
         edit: false
     }
 
-    //GET request
     //PUT request
     editDependent = () => {
         console.log('edit dependent:', this.props.item) //this will target the specific dependent clicked
@@ -41,56 +40,65 @@ class AccountsItem extends Component {
         let dateOfBirth = <Moment format="MM/DD/YYYY">{this.props.item.date_of_birth}</Moment>
         return (
             <>
-            {this.state.edit === false ?
-                <tr>
-                    <td>{this.props.item.first_name} {this.props.item.first_name}</td>
-                    <td>{this.props.item.phone_number}</td>
-                    <td>{dateOfBirth}</td>
-                    {/* address td will be a concatenated string of building_address 1 and 2 */}
-                    <td>{this.props.item.building_address1} {this.props.item.building_address2}</td>
-                    <td>{this.props.item.zip_code}</td>
-                    <td>{this.props.item.county_name}</td>
-                    <td>{this.props.item.city}</td>
-                    <td>{this.props.item.special_request}</td>
-                    <td>{this.props.item.menu_description}</td>
-                    <td>{this.props.item.dietary_restrictions}</td>
-                    <td>{this.props.item.referral_name}</td>
-                    <td>{this.props.item.program_name}</td>
-                    {/* if program === ramsey county return a yes/no if else return empty */}
-                    {this.props.item.program_id === 1 ?
-                        <>
-                            <td>YES/NO</td>
-                        </>
-                        :
-                        <>
-                            <td>n/a</td>
-                        </>
-                    }
-                    {/* this will conditionally render all information to inputs */}
-                    <td><button onClick={this.toggleEdit}>Edit</button><button onClick={this.updateDependent}>Update</button></td>
-                </tr>
-                :
-                <tr>
-                    {/* need to add last name to toggle value */}
-                <td><input value={this.props.item.first_name}/></td>
-                <td><input value={this.props.item.phone_number}/></td>
-                <td><input value={dateOfBirth}/></td>
-                {/* address value will need to be a concatenated string of building_address 1 and 2 */}
-                <td><input value={this.props.item.building_address1}/></td>
-                <td>55413</td>
-                <td>Hennepin</td>
-                <td>Minneapolis</td>
-                <td>No meat</td>
-                <td>Veggie</td>
-                <td></td>
-                <td>MN Senior Center</td>
-                <td>Meals On Wheels</td>
-                <td>Yes</td>
-                {/* this will conditionally render all information to inputs */}
-                <td><button onClick={this.toggleEdit}>Edit</button><button onClick={this.updateDependent}>Update</button></td>
-            </tr>
+                {this.state.edit === false ?
+                    <tr>
+                        <td>{this.props.item.first_name} {this.props.item.first_name}</td>
+                        <td>{this.props.item.phone_number}</td>
+                        <td>{dateOfBirth}</td>
+                        {/* address td will be a concatenated string of building_address 1 and 2 */}
+                        <td>{this.props.item.building_address1} {this.props.item.building_address2}</td>
+                        <td>{this.props.item.zip_code}</td>
+                        <td>{this.props.item.county_name}</td>
+                        <td>{this.props.item.city}</td>
+                        <td>{this.props.item.special_request}</td>
+                        <td>{this.props.item.menu_description}</td>
+                        <td>{this.props.item.dietary_restrictions}</td>
+                        <td>{this.props.item.referral_name}</td>
+                        <td>{this.props.item.program_name}</td>
+                        {/* if program === ramsey county return a yes/no if else return empty */}
+                        {this.props.item.program_id === 1 ?
+                            <>
+                                <td>YES/NO</td>
+                            </>
+                            :
+                            <>
+                                <td>n/a</td>
+                            </>
+                        }
+                        {/* this will conditionally render all information to inputs */}
+                        <td><button onClick={this.toggleEdit}>Edit</button><button onClick={this.updateDependent}>Update</button></td>
+                    </tr>
+                    :
+                    <tr>
+                        {/* need to add last name to toggle value */}
+                        <td><input value={this.props.item.first_name} /></td>
+                        <td><input value={this.props.item.phone_number} /></td>
+                        <td><input value={dateOfBirth} /></td>
+                        {/* address value will need to be a concatenated string of building_address 1 and 2 */}
+                        <td><input value={this.props.item.building_address1} /></td>
+                        <td><input value={this.props.item.zip_code} /></td>
+                        <td><input value={this.props.item.county_name} /></td>
+                        <td><input value={this.props.item.city} /></td>
+                        <td><input value={this.props.special_request} /></td>
+                        <td><input value={this.props.menu_description} /></td>
+                        <td><input value={this.props.dietary_restrictions} /></td>
+                        <td><input value={this.props.item.referral_name} /></td>
+                        <td><input value={this.props.item.program_name} /></td>
+                        <td><input value={this.props.item.program_id === 1 ?
+                            <>
+                            {/* this will need a true/false based on the new table update if true return yes/ if false return no/ default is false */}
+                                <td>YES/NO</td>
+                            </>
+                            :
+                            <>
+                                <td></td>
+                            </>
+                        } /></td>
+                        {/* this will conditionally render all information to inputs */}
+                        <td><button onClick={this.toggleEdit}>Edit</button><button onClick={this.updateDependent}>Update</button></td>
+                    </tr>
 
-            }
+                }
             </>
         );//end return
     }//end render
