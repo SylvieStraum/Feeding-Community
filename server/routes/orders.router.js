e66a34633fed21245ba6a6d03b2aa9f279b68843
Reelshrimpboat <Camneelycreative@gmail.com>
2020-08-01 13:30:04 -0500

    date range router, sagas started, db orders change

--- server/routes/orders.router.js
+++ server/routes/orders.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectNotAdmin } = require('../modules/admin-authentication-middleware');
+const { query } = require('../modules/pool');
 
 // GET ROUTE to get all orders
 router.get('/', rejectNotAdmin, (req, res) => {
@@ -10,7 +11,7 @@ router.get('/', rejectNotAdmin, (req, res) => {
                         ORDER BY "date" ASC;`;
     pool.query(queryText)
         .then((result) => {
-            console.log(`GET Ratings database request successful`, result);
+            console.log(`GET database request successful`);
             res.send(result.rows);
         })
         .catch((error) => {
@@ -25,6 +26,7 @@ router.get('/today/', rejectNotAdmin, (req, res) => {
 
     let today = new Date();
     let date = '';
+    console.log('today:', today)
     if (today.getMonth() < 10) {
         if (today.getDate() < 10) {
             date = today.getFullYear() + '-0' + (today.getMonth() + 1) + '-0' + today.getDate();
@@ -38,14 +40,14 @@ router.get('/today/', rejectNotAdmin, (req, res) => {
             date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
         }
     }
-    console.log(date);
+    console.log('date:' , date);
 
     const queryText = `SELECT * FROM "orders"
                         WHERE "date" = $1
                         ;`;
     pool.query(queryText, [date])
         .then((result) => {
-            console.log(`GET Ratings database request successful`, result);
+            console.log(`GET database request successful`);
             res.send(result.rows);
         })
         .catch((error) => {
@@ -54,33 +56,54 @@ router.get('/today/', rejectNotAdmin, (req, res) => {
         });
 }); // END GET ROUTE
 
-// GET ROUTE to get specifc days
-// router.get('/dates/', rejectNotAdmin, (req, res) => {
+// GET ROUTE to get a month's worth of dates
+router.get('/month/', rejectNotAdmin, (req, res) => {
 
-//     let date = `%${req.body.date}%`
+    let date = `%${req.body.date}%`
 
-//     console.log(date);
+    console.log(date);
 
-//     const queryText = `SELECT * FROM "orders"
-//                         WHERE "date"::text LIKE $1
-//                         ;`;
-//     pool.query(queryText, [date])
-//         .then((result) => {
-//             console.log(`GET Ratings database request successful`, result);
-//             res.send(result.rows);
-//         })
-//         .catch((error) => {
-//             console.log(`Error making GET Request:`, error);
-//             res.sendStatus(500);
-//         });
-// }); // END GET ROUTE
+    const queryText = `SELECT * FROM "orders"
+                        WHERE "date"::text LIKE $1
+                        ;`;
+    pool.query(queryText, [date])
+        .then((result) => {
+            console.log(`GET database request successful`);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making GET Request:`, error);
+            res.sendStatus(500);
+        });
+}); // END GET ROUTE
 
-// GET ROUTE to get specifc days 2
+// GET ROUTE to get a year's worth of dates
+router.get('/year/', rejectNotAdmin, (req, res) => {
+
+    let date = `%${req.body.date}%`
+
+    console.log(date);
+
+    const queryText = `SELECT * FROM "orders"
+                        WHERE "date"::text LIKE $1
+                        ;`;
+    pool.query(queryText, [date])
+        .then((result) => {
+            console.log(`GET database request successful`);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making GET Request:`, error);
+            res.sendStatus(500);
+        });
+}); // END GET ROUTE
+
+// GET ROUTE to get specifc range
 router.get('/dates/', rejectNotAdmin, (req, res) => {
 
-    console.log('query:' , req.query)
-    console.log('startDate:' , req.query.startDate)
-    console.log('endDate:' , req.query.endDate)
+    // console.log('query:' , req.query)
+    // console.log('startDate:' , req.query.startDate)
+    // console.log('endDate:' , req.query.endDate)
 
     function getDates(startDate, endDate) {
 
@@ -89,28 +112,34 @@ router.get('/dates/', rejectNotAdmin, (req, res) => {
 
         // sets current date to date type
         let currentDate = new Date(startDate);
+        console.log('currentDate:', currentDate);
 
         // sets stop date to date type
         let stopDate = new Date(endDate);
+        console.log('stopDate:', stopDate);
 
         // while loop to extract dates
         while (currentDate <= stopDate) {
             // sets current Date to date for shorter writting within loop
             let date = currentDate
-
+            // console.log('date:', date);
             // if else to check whether month and day return would be single digit
             // if single digit adds '0' before to keep naming convention
             if (date.getMonth() < 10) {
                 if (date.getDate() < 10) {
                     date = date.getFullYear() + '-0' + (date.getMonth() + 1) + '-0' + date.getDate();
+                    // console.log('in if, date:', date);
                 } else {
                     date = date.getFullYear() + '-0' + (date.getMonth() + 1) + '-' + date.getDate();
+                    // console.log('in if, date:', date);
                 }
             } else {
                 if (today.getDate() < 10) {
                     date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-0' + date.getDate();
+                    // console.log('in if, date:', date);
                 } else {
                     date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+                    // console.log('in if, date:', date);
                 }
             }
 
@@ -123,42 +152,43 @@ router.get('/dates/', rejectNotAdmin, (req, res) => {
     }
     let dates = getDates(req.query.startDate, req.query.endDate);
     console.log('dates', dates);
-    let queryText = 
+    let queryText = 'SELECT * FROM "orders" WHERE '
     function queryDefine (input){
         let whereLength = input.length
         console.log('whereLength:', whereLength);
+        for (let index = 1; index <= input.length; index++) {
+            if(index === 1){
+                queryText = queryText.concat(` "date" = ${'$' + index}`)
+            }else if(index === input.length){
+                queryText = queryText.concat(` OR "date" = ${'$' + index} ORDER BY "date" ASC;`)
+            }
+            else{
+                queryText = queryText.concat(` OR "date" = ${'$' + index}`)
+            }
+        }
     }
     queryDefine(dates);
-    res.sendStatus(200);
-    // const queryText = `SELECT * FROM "orders"
-    //                     WHERE "date"::text LIKE $1
-    //                     ;`;
-    // pool.query(queryText, [date])
-    //     .then((result) => {
-    //         console.log(`GET Ratings database request successful`, result);
-    //         res.send(result.rows);
-    //     })
-    //     .catch((error) => {
-    //         console.log(`Error making GET Request:`, error);
-    //         res.sendStatus(500);
-    //     });
+    console.log(queryText);
+
+    pool.query(queryText, dates)
+        .then((result) => {
+            console.log(`GET database request successful`);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making GET Request:`, error);
+            res.sendStatus(500);
+        });
 }); // END GET ROUTE
 
 //POST ROUTE to save current meals from the day to orders table
 router.post('/save-day/', rejectNotAdmin, (req, res) => {
     
 
-    const queryText = `WITH select_json AS (
-                            SELECT  
-                                JSON_AGG(
-                                    JSON_BUILD_OBJECT('id', d.id, 'first_name', d.first_name, 'last_name', d.last_name, 'building_address1', d.building_address1, 'building_address2', d.building_address2, 'number_of_meals', m.number_of_meals, 'meal_choice', m.meal_choice)
-                                ) AS "json_arry" 
-                            FROM dependents d
-                            JOIN current_meal m ON d.id = m.dependent_id)
-                            INSERT INTO "orders"("date", "daily_orders")
-                            SELECT current_timestamp, select_json.json_arry
-                            FROM select_json
-                            ;`;
+    const queryText = `INSERT INTO "orders" ("date", "dependent_id", "number_of_meals", "meal_choice")
+                        SELECT current_timestamp, "dependent_id", "number_of_meals", "meal_choice" FROM "current_meal"
+                        ORDER BY "dependent_id" ASC
+                        ;`;
 
     
     console.log('put request:,', queryText)
--- src/redux/sagas/ordersSaga.js
+++ src/redux/sagas/ordersSaga.js
@@ -11,8 +11,39 @@ function* getTodaysOrders() {
     }
 }
 
-//retrieves todays orders
+//retrieves a specific month's orders
+function* getMonthOrders() {
+    try {
+        const responsePayload = yield axios.get(`/api/orders/month/${action.payload.id}`);
+        yield put({
+            type: 'SET_ORDERS',
+            payload: responsePayload
+        });
+    } catch (error) {
+        console.log('Get orders saga error', error);
+    }
+}
+
+//retrieves a specific years's orders
+function* getYearOrders() {
+    try {
+        const responsePayload = yield axios.get(`/api/orders/year/${action.payload.id}`);
+        yield put({
+            type: 'SET_ORDERS',
+            payload: responsePayload
+        });
+    } catch (error) {
+        console.log('Get orders saga error', error);
+    }
+}
+
+//retrieves range of orders
 function* getDatesOrders(action) {
+    // note for future cam/other devs to get dates action.payload.id will have to be a http params query like:
+    // ?startDate=2020-07-30:01:00:00&endDate=2020-08-01:01:00:00
+    // startDate determines the starting date, endDate determines ending date
+    // YYYY-MM-DD:HH:MM:SS format works well with this system
+    // HH:MM:SS has to be specified or it defaults to 00:00:00, which can end up with date being read as day before
     try {
         const responsePayload = yield axios.get(`/api/orders/dates/${action.payload.id}`);
         yield put({
@@ -38,6 +69,8 @@ function* alterOrders(action) {
 
 function* menuSaga() {
     yield takeEvery('GET_TODAYS_ORDERS', getTodaysOrders);
+    yield takeEvery('GET_MONTH_ORDERS', getMonthOrders);
+    yield takeEvery('GET_YEAR_ORDERS', getYearOrders);
     yield takeEvery('GET_DATE_RANGE_ORDERS', getDatesOrders);
     yield takeEvery('UPDATE_TODAYS_ORDERS', alterOrders);
 }
